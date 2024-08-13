34cf1940840d772d979057616252d0b76d47a060
Prof. Márcio Torres <marcio.torres@riogrande.ifrs.edu.br>
2024-08-13 18:55:29 -0300

    Create chamada.rb

---
+++ chamada.rb
@@ -0,0 +1,29 @@
+presentes = %w{
+  Luã
+  David
+  Bruno
+  Vinícius
+  Gustavo
+  Evelyn
+  Douglas
+  JoãoS
+  Ronaldo
+  Felipe
+  Fellipe
+  Alan
+  Jaifer
+  Pablo
+  Arthur
+  Lucas
+  Mathias
+  Adriano
+  Daniel
+  Julia
+  JoãoB
+}
+
+presentes.shuffle!
+
+until presentes.empty?
+  p presentes.pop(2)
+end
